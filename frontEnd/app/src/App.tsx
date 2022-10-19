9992c5b7a388a8434082f9330989b60155b19ef3
SnarkosaurusRex <lydiaholtrop@gmail.com>
2022-10-18 23:42:19 -0400

    replace starting template with basic page outline and placeholders

--- frontEnd/app/src/App.tsx
+++ frontEnd/app/src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.scss';
+import AddPersonForm, { PersonData } from './Components/AddPersonForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [groupMembers, setGroupMembers] = useState<PersonData[]>([]);
+
+    return (
+        <div className="page-wrapper">
+            <div className="top-content">
+                <h1>Welcome to the Gift Exchanginator!</h1>
+                <div className="instructions">Information about how this works/what to do with it/etc. will go here.</div>
+            </div>
+
+            <div className="add-person-section">
+                <h2>{'Add a Group Member'}</h2>
+                <AddPersonForm
+                    onSubmit={(newPerson) => setGroupMembers(groupMembers.concat([newPerson]))}
+                />
+            </div>
+
+            <div className="group-members-section">
+                <h2>{'Group Members'}</h2>
+                {'list of current group members will be displayed here'}
+            </div>
+            
+            <div className="options-section">
+                <h2>{'Options'}</h2>
+                {'options will be displayed here'}
+            </div>
+
+            <button>{'Run'}</button>
+        </div>
+    );
 }
 
 export default App;
